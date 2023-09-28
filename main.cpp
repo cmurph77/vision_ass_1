5f771a11fa1500b231e95d1a18ec80131777c46d
cmurph77 <91293683+cmurph77@users.noreply.github.com>
2023-09-28 23:39:00 +0100

    setting up code for starting part 1

--- main.cpp
+++ main.cpp
@@ -10,19 +10,16 @@ using namespace std;
 
 
 
-void convert_to_binary(){
-    string path = "/Users/cianmurphy/Desktop/OpenCV/monalisa.jpeg";
-    string sign_path = "/Users/cianmurphy/Desktop/OpenCV/images/yellow_sign.png";
-    Mat original_image = imread(sign_path);
+Mat convert_to_binary(Mat original_image){
     Mat grayscale_image;//declaring a matrix to store grayscale image//
     Mat binary_image;//declaring a matrix to store the binary image
-    namedWindow("Original Image");//declaring window to show binary image//
-    namedWindow("Show Binary");//declaring window to show original image//
+//    namedWindow("Original Image");//declaring window to show binary image//
+//    namedWindow("Show Binary");//declaring window to show original image//
     cvtColor(original_image, grayscale_image, COLOR_BGR2GRAY);//Converting BGR to Grayscale image and storing it into converted matrix//
-    threshold(grayscale_image, binary_image, 100, 255, THRESH_BINARY);//converting grayscale image stored in converted matrix into binary image//
-    imshow("Original Image", original_image);//showing Original Image//
-    imshow("Show Binary", binary_image);//showing Binary Image//
-
+    threshold(grayscale_image, binary_image, 180, 255, THRESH_BINARY);//converting grayscale image stored in converted matrix into binary image//
+    //imshow("Original Image", original_image);//showing Original Image//
+    //imshow("Show Binary", binary_image);//showing Binary Image//
+    return binary_image;
 }
 
 void find_letters(){
@@ -38,18 +35,17 @@ void find_letters(){
 
 }
 
-void find_text_solution(){
-    //convert to greyscale
-    // threshold the image
-    // CCA - Connected Components Analysis
-    // large region with holes OR
+void get_ball_location(Mat img){
+
+}
+
+void part1(){
+    cout<<"Finding the location of the ball in image 1";
+
 }
 
 int main() {
-
-    //convert_to_binary();
-    find_letters();
-    find_text_solution();
+    part1();
     waitKey(0);
     return 0;
 }
