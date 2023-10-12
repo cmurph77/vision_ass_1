9b1f51ed81ead0c558de3bfd54e36b32775fb7c4
cmurph77 <91293683+cmurph77@users.noreply.github.com>
2023-10-12 20:16:40 +0100

    part 1 nearly working. Just need to verify balls.

--- part_1.cpp
+++ part_1.cpp
@@ -1,40 +1,91 @@
-// This C++ code utilizes the OpenCV library to perform the following operations on an input image:
-//
-//Load an image.
-//Convert the image to grayscale.
-//Blur the grayscale image using a 3x3 kernel.
-//Apply the Hough Circle Transform to detect circles in the blurred grayscale image.
-//Draw the detected circles on the original color image:
-//Circumference of the detected circles in green.
-//Centers of the detected circles in red.
-//Display the image with the detected circles.
-//The Hough Circle Transform is used to identify circles in the image, and the detected circles are then
-// visualized on the original image to illustrate the detected features.
 
+bool isColorOrange(cv::Mat hsv_color) {
+    // Define the lower and upper bounds for the orange color in HSV space
+    cv::Scalar orangeLower(15, 100, 100);  // Lower bound for orange in HSV
+    cv::Scalar orangeUpper(30, 255, 255);  // Upper bound for orange in HSV
+
+    // Create an empty mask image of the same size as the input HSV image
+    cv::Mat mask(hsv_color.size(), CV_8U);
+
+    // Check if the provided HSV color is within the specified range for orange
+    cv::inRange(hsv_color, orangeLower, orangeUpper, mask);
+
+    // Check if any pixel is in the range (i.e., if any pixel in the mask is non-zero)
+    return cv::countNonZero(mask) > 0;
+}
+
+bool isColorWhite(cv::Mat hsv_color) {
+    // Define the lower and upper bounds for the orange color in HSV space
+    cv::Scalar whiteLower(0,0,200);
+    cv::Scalar whiteUpper(180,100,255);
+
+    // Create an empty mask image of the same size as the input HSV image
+    cv::Mat mask(hsv_color.size(), CV_8U);
+
+    // Check if the provided HSV color is within the specified range for orange
+    cv::inRange(hsv_color, whiteLower, whiteUpper, mask);
+
+    // Check if any pixel is in the range (i.e., if any pixel in the mask is non-zero)
+    return cv::countNonZero(mask) > 0;
+}
+
+Mat detect_color(Mat img, double x, double y, double r){
+    cv::Point center(x, y); // create point object for circle center
+    // Draw the circumference of the circle
+    cv::circle(img, center, r, cv::Scalar(0, 255, 0), 2);
+    // Draw a small circle (of radius 1) to show the center
+    cv::circle(img, center, 1, cv::Scalar(0, 0, 255), 3);
+    //imshow("Image with Circle",img);
+
+    // Detecting Color
+    // Create a mask for the circle
+    cv::Mat mask(img.size(), CV_8U, cv::Scalar(0));
+    cv::circle(mask, center, r, cv::Scalar(255), -1);  // Filled circle
+    //imshow("Mask",mask);
+
+    // Calculate the mean color inside the circle using the mask
+    cv::Scalar meanColor = cv::mean(img, mask);
+
+    // CONVERT TO HSV
+    cv::Mat meanColorBGR(1, 1, CV_8UC3, meanColor);
+    cv::Mat meanColorHSV;
+    cv::cvtColor(meanColorBGR, meanColorHSV, cv::COLOR_BGR2HSV);
+
+    if(isColorOrange(meanColorHSV)) {
+        //std::cout<< "TRUE";
+         };
+
+    // Print the average color values (BGR format)
+    //std::cout << "Average color (BGR): " << meanColor[2] << ", " << meanColor[1] << ", " << meanColor[0] << ". "<< std::endl;
+
+    return meanColorHSV;
+
+}
+
+// TODO -> return the coords and center from this function
 void edge_detection(Mat img){
-    //std:cout << "doing edge detection";
-    imshow("origninal image",img);
+    //imshow("origninal image",img);
 
     // Convert to grayscale
     cv::Mat gray;
     cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
 
-    // Blur using a 3x3 kernel
+    // Blur using a kernel specified by the size of blur_matrix variable below
     cv::Mat grayBlurred;
-    int blur_matrix = 10;
+    int blur_matrix = 7;
     cv::blur(gray, grayBlurred, cv::Size(blur_matrix, blur_matrix));
 
     // Apply Hough transform on the blurred image
     int maxBallRadius = 100;
     int minDist = grayBlurred.rows / 8;
-    std::vector<cv::Vec3f> detectedCircles;
+    std::vector<cv::Vec3f> detectedCircles; // store the detected circles here
     cv::HoughCircles(grayBlurred, detectedCircles, cv::HOUGH_GRADIENT, 1,
                      minDist,  // change this value to detect circles with different distances to each other
                      50, 30, 1, maxBallRadius);
 
     // Draw circles that are detected
     for (size_t i = 0; i < detectedCircles.size(); i++){
-        //std::cout << "Circles Detected";
+        std::cout << "\n  - Circle: " << (i+1) << "." ;
         cv::Point center(cvRound(detectedCircles[i][0]), cvRound(detectedCircles[i][1])); // firse two vals in circle vector is corrds
         int radius = cvRound(detectedCircles[i][2]); // third value is radius
 
@@ -43,34 +94,51 @@ void edge_detection(Mat img){
 
         // Draw a small circle (of radius 1) to show the center
         cv::circle(img, center, 1, cv::Scalar(0, 0, 255), 3);
-        std::cout << ": (" << center.x << ", " << center.y << ")" << std::endl;
+        std::cout << "\n      -> Center : (" << center.x << ", " << center.y << ")" ;
+
+        cv::Mat hsvBallColor = detect_color(img,center.x,center.y,radius);
+        bool orange = isColorOrange(hsvBallColor);
+        if(orange){
+            std::cout << "\n      -> Ball is Orange" << (i + 1); }
+        bool white = isColorWhite(hsvBallColor);
+        if(orange){
+            std::cout << "\n      -> Ball is White" << (i + 1); }
+
 
     }
     cv::imshow("Detected Circles", img);
 
 }
+
+
+
+//This function returns the mean color of the circle in the image supplied
 void find_center(int image_no){
-    //cout<<"Finding the location of the ball in image" << image_no;
     string path = "/Users/cianmurphy/code_directories/vision_ass_1/balls/Ball" + to_string(image_no) + ".jpg";  // TODO add a constant for the start of the path
     Mat ball_img = imread(path); // read in ball image from the path
-    imshow("Ball " + to_string(image_no)+"  Image" ,ball_img);  // display original image
-
-    /// ******* ENTER THE FUNCTION YOU WANT TO USE TO FIND BALL CENTER ***************
-    //clustering_attempt(ball_img);
+    //imshow("Ball " + to_string(image_no)+"  Image" ,ball_img);  // display original image
     edge_detection(ball_img);
 
 }
 
 void iterate_through_ball_images(){
     for (int i = 1; i < 10; ++i) {
-        std::cout << "Image " << i << "";
+        std::cout << "\n\nImage " << i << "";
         find_center(i);
     }
 }
 
+//void ball_detection_test(){
+//    string path = "/Users/cianmurphy/code_directories/vision_ass_1/balls/Ball8.jpg";  // TODO add a constant for the start of the path
+//    Mat img = imread(path);
+//    detect_color(img,523.5,	458.5	,61/2);
+//
+//}
+
 void part_1(){
     //todo back project the circle and verify if white or orange?
     iterate_through_ball_images();
-    //find_center(8);
+    //find_center(9);
+    //ball_detection_test();
 
 }
